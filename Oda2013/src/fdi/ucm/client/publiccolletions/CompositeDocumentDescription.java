2b8ee733ac9315fce9069f49ad9fd006265b3376
Joaquin Gayoso Cabada <gayoxo@gmail.com>
2014-01-25 16:36:51 +0100

    gogogogogo

--- Oda2013/src/fdi/ucm/client/publiccolletions/CompositeDocumentDescription.java
+++ Oda2013/src/fdi/ucm/client/publiccolletions/CompositeDocumentDescription.java
@@ -4,7 +4,8 @@
 package fdi.ucm.client.publiccolletions;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+
 import com.google.gwt.user.client.ui.Composite;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -22,9 +23,7 @@ import com.google.gwt.user.client.ui.Image;
 
 import fdi.ucm.client.controller.Oda2013OperatinoalViewStaticFunctions;
 import fdi.ucm.shared.model.collection.document.Documents;
-import fdi.ucm.shared.model.collection.document.Element;
 import fdi.ucm.shared.model.collection.grammar.ElementType;
-import fdi.ucm.shared.model.collection.grammar.Iterator;
 import fdi.ucm.shared.model.collection.grammar.Structure;
 
 /**
@@ -37,12 +36,12 @@ public class CompositeDocumentDescription extends Composite {
 	private static final String SHOWMORE = "Show More";
 	protected static final int MaxWidth = 50;
 	private static final String PX = "PX";
-	private HashMap<ElementType, VerticalPanel> Pestanas;
+//	private HashMap<ElementType, VerticalPanel> Pestanas;
 	private SimplePanel PanelDecorador;
-	private HashMap<Element,CompositeTabElement> Procesados;
+//	private HashMap<Element,CompositeTabElement> Procesados;
 	private Documents Recurso;
-	private ArrayList<Element> vaciosCreados;
-	private ArrayList<ElementType> MetaOrden;
+//	private ArrayList<Element> vaciosCreados;
+//	private ArrayList<ElementType> MetaOrden;
 	private Image Icono;
 	private String Destino;
 	private String ImagenAsociada;
@@ -54,12 +53,11 @@ public class CompositeDocumentDescription extends Composite {
 
 	public CompositeDocumentDescription(Documents recurso) {
 		super();
-		Pestanas=new HashMap<ElementType, VerticalPanel>();
-		
-		Procesados=new HashMap<Element,CompositeTabElement>();	
+//		Pestanas=new HashMap<ElementType, VerticalPanel>();
+//		Procesados=new HashMap<Element,CompositeTabElement>();	
 		Recurso=recurso;
-		vaciosCreados=new ArrayList<Element>();
-		MetaOrden=new ArrayList<ElementType>();
+//		vaciosCreados=new ArrayList<Element>();
+//		MetaOrden=new ArrayList<ElementType>();
 //		MetaPestanaATab=new HashMap<Meta, TabElement>();
 		
 		
@@ -162,10 +160,60 @@ public class CompositeDocumentDescription extends Composite {
 		
 
 	
-		
+		Refresh();
 		
 	
 		
+//		PanelA = new VerticalPanel();
+//		PanelA.setSize("100%", "100%");
+//		
+//		PanelDecorador.add(PanelA);
+//		
+//		Composite Descripcion=new CompositeDocumentDescriptionDescriptionTab(Recurso.getDescriptionText());
+//		PanelA.add(Descripcion);
+//		
+//		
+//		for (Structure MetaElem : Recurso.getDocument().getSons()) {
+//				
+//			if (MetaElem instanceof ElementType)
+//				{
+//				
+//				Pestanas.put((ElementType) MetaElem, PanelA);
+//				MetaOrden.add((ElementType) MetaElem);
+//				}
+//			else 
+//				{
+//				 procesaEstrella((Iterator)MetaElem);
+//				}
+//			
+//
+//		}
+//		
+//		
+//	
+//		
+//		for (Element MetaValueD : recurso.getDescription()) {
+//			if (!Procesados.containsKey(MetaValueD)&&(Oda2013OperatinoalViewStaticFunctions.isSummary(MetaValueD)))
+//					procesa(MetaValueD);
+//		}
+//		
+		
+
+	}
+
+
+
+
+
+	/**
+	 * Refresca los datos.
+	 */
+	private void Refresh() {
+
+
+		PanelDecorador.clear();
+		
+
 		PanelA = new VerticalPanel();
 		PanelA.setSize("100%", "100%");
 		
@@ -174,293 +222,278 @@ public class CompositeDocumentDescription extends Composite {
 		Composite Descripcion=new CompositeDocumentDescriptionDescriptionTab(Recurso.getDescriptionText());
 		PanelA.add(Descripcion);
 		
-		
-		for (Structure MetaElem : Recurso.getDocument().getSons()) {
-				
-			if (MetaElem instanceof ElementType)
-				{
-				
-				Pestanas.put((ElementType) MetaElem, PanelA);
-				MetaOrden.add((ElementType) MetaElem);
-				}
-			else 
-				{
-				 procesaEstrella((Iterator)MetaElem);
-				}
-			
-
-		}
-		
-		
-	
-		
-		for (Element MetaValueD : recurso.getDescription()) {
-			if (!Procesados.containsKey(MetaValueD)&&(Oda2013OperatinoalViewStaticFunctions.isSummary(MetaValueD)))
-					procesa(MetaValueD);
-		}
+		processSons(Recurso.getDocument().getSons(),PanelA);
 		
 		
 
+		
 	}
-
 
 
 
 
 
 	/**
-	 * Funcion que procesa un recurso, creando el padre si este no existe.
-	 * @param metaValueD
+	 * Procesa los hijos a tratar sobre el panel que se incorporaran
+	 * @param list
+	 * @param panelA2
 	 */
-	private CompositeTabElement procesa(Element metaValueD) {
-		
-		if (Pestanas.containsKey(metaValueD.getHastype()))
-		{
-			VerticalPanel A = Pestanas.get(metaValueD.getHastype());
-			CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
-			A.add(nuevo);
-			Procesados.put(metaValueD, nuevo);
-//			MetaPestanaATab.put(metaValueD.getHastype(), nuevo);
-			return nuevo;
-		}
-		else
-		{
-			ElementType Padre;
+	private void processSons(List<Structure> list, VerticalPanel panelA2) {
+		for (Structure elementoHijo : list) {
 			
-			Padre=FindPadreSummaryAscendente(metaValueD, metaValueD.getHastype());
-			
-			if (Pestanas.containsKey(Padre))
-				if (isSummaryMeta(Padre,metaValueD))
-					{
-					//Se retorno el padre por ser el padre
-						return procesosobremipadre(metaValueD,Padre);
-					}
-				else
-				{
-					Pestanas.put((ElementType) metaValueD.getHastype(), PanelA);
-					MetaOrden.add((ElementType) metaValueD.getHastype());
-					CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
-					PanelA.add(nuevo);
-					Procesados.put(metaValueD, nuevo);
-					return nuevo;
-				}
-			else
-				{
-				return procesosobremipadre(metaValueD,Padre);
-				}
-			
-			
-			
-			}	
-		
-	}
-
-
-	/**
-	 * procesa si el padre es visible
-	 * @param padre
-	 * @param metaValueD 
-	 * @return
-	 */
-	private boolean isSummaryMeta(ElementType padre, Element metaValueD) {
-		Element PadreMV=findMetaValue(padre, metaValueD.getAmbitos());
-		if (PadreMV!=null)
-			return Oda2013OperatinoalViewStaticFunctions.isSummary(PadreMV);
-		else
-			return Oda2013OperatinoalViewStaticFunctions.isSummary(padre);
-				
-	}
-
-
-
-
-
-
-	/**
-	 * Procesa un elemto sobre su padre
-	 * @param metaValueD
-	 * @param padre
-	 * @return 
-	 */
-	private CompositeTabElement procesosobremipadre(Element metaValueD, ElementType padre) {
-		Element PadreValue=findMetaValue(padre,metaValueD.getAmbitos());
-		
-		CompositeTabElement PadreTab;
-		if (PadreValue==null)
-		{
-			
-			Element MV=getMetavalueVacio(padre,metaValueD.getAmbitos());
-			if (MV==null)
+			if (elementoHijo instanceof ElementType && Oda2013OperatinoalViewStaticFunctions.isSummary((ElementType)elementoHijo))
 			{
-				MV = new Element(padre);
-				for (Integer iterable_element :metaValueD.getAmbitos()) {
-					MV.getAmbitos().add(iterable_element.intValue());
-				}
-				vaciosCreados.add(MV);
-				PadreTab=procesa(MV);
-				
-			}
-			else
-			{
-				PadreTab=Procesados.get(MV);
-			}
-		
-		}
-		else{
-		
-		 PadreTab = Procesados.get(PadreValue);
-
-		 if (PadreTab==null)
-				{
-			 		PadreTab=procesa(PadreValue);
-				}
-		}
-		
-		VerticalPanel A = PadreTab.getPanelHijos();	
-		CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
-		A.add(nuevo);
-		Procesados.put(metaValueD, nuevo);
-		return nuevo;
-		
-	}
-
-	/**
-	 * Busca al padre que ademas de se meta sea summary
-	 * @param metaValueD
-	 * @return
-	 */
-	private ElementType FindPadreSummaryAscendente(Element metaValueD,ElementType Metain) {
-		
-		ElementType Padre;
-		
-		if (Metain.getFather()==null)
-			return Metain;
-		
-		if (Metain.getFather() instanceof ElementType)
-			Padre=(ElementType) Metain.getFather();
-		else 
-			Padre=findMeta((Iterator) Metain.getFather());
-		
-		Element PadreMV=findMetaValue(Padre, metaValueD.getAmbitos());
-		
-		
-		
-		if (PadreMV!=null)
-			if (Oda2013OperatinoalViewStaticFunctions.isSummary(PadreMV))
-				return Padre;
-			else 
-				return FindPadreSummaryAscendente(PadreMV,PadreMV.getHastype());
-		else
-			if (Oda2013OperatinoalViewStaticFunctions.isSummary(Padre))
-				return Padre;
-			else 
-				return FindPadreSummaryAscendente(metaValueD,Padre);
-
-	}
-
-
-
-
-
-	private Element getMetavalueVacio(ElementType padre, ArrayList<Integer> ambitos) {
-		for (Element vaciosCandidato : vaciosCreados) {
-			if ((vaciosCandidato.getHastype()==padre)&&(checkAmbitos(vaciosCandidato.getAmbitos(),ambitos)))
-				return vaciosCandidato;
-		}
-		
-		return null;
-	}
-
-	/**
-	 * Busca un MetaValue en la lista de recursos
-	 * @param element
-	 * @param AmbitosElementoBase
-	 * @return
-	 */
-	private Element findMetaValue(ElementType element, ArrayList<Integer> AmbitosElementoBase) {
-		for (Element metaValuePro : Recurso.getDescription()) {
-			if ((metaValuePro.getHastype()==element)&&(checkAmbitos(metaValuePro.getAmbitos(),AmbitosElementoBase)))
-				return metaValuePro;
-		}
-		return null;
-	}
-
-	
-	/**
-	 * Chequea si las dos listas de ambitos coinciden, la segunda es la que ha de ser mayor ( mas profunda ) que la primera ( padre )
-	 * @param ambitos ambitos del padre a chequear
-	 * @param AmbitosElementoBase ambitos del hijo
-	 * @return
-	 */
-	private boolean checkAmbitos(ArrayList<Integer> ambitos,
-			ArrayList<Integer> AmbitosElementoBase) {
-		
-		if (ambitos.size()>AmbitosElementoBase.size())
-			return false;
-		
-		for (int i = 0; i < AmbitosElementoBase.size(); i++) {
+				CompositeDocumentDescriptionTabStructureSummary T=new CompositeDocumentDescriptionTabStructureSummary((ElementType)elementoHijo,Recurso,new ArrayList<Integer>());
+				panelA2.add(T);
+				processSons(elementoHijo.getSons(), T.getPanelHijos());
+			} 
+			else  processSons(elementoHijo.getSons(), panelA2);
 			
-			if ((ambitos.size()>i)&&(AmbitosElementoBase.get(i)!=null)&&(ambitos.get(i)!=null)&&(AmbitosElementoBase.get(i).intValue()!=ambitos.get(i).intValue()))
-				return false;
-		}
-		
-		return true;
-	}
-
-	/**
-	 * Busca el padre del elemento en Meta mas cercano hacia arriba en el arbol
-	 * @param IteradorProcesar
-	 * @return Meta mas cercano hacia arrriba en el arbol.
-	 */
-	private ElementType findMeta(Iterator IteradorProcesar) {
-		if (IteradorProcesar.getFather()==null)
-			return null;
-		if (IteradorProcesar.getFather() instanceof ElementType)
-			return (ElementType) IteradorProcesar.getFather();
-		else return findMeta((Iterator)IteradorProcesar.getFather());
-	}
-
-
-	/**
-	 * Mecanismo de proceso de un atributo estrella para la creacion de las pestañas
-	 * @param metaElem
-	 */
-	private void procesaEstrella(Iterator metaElem) {
-		for (Structure MetaElem : metaElem.getSons()) {
-			
-			if (MetaElem instanceof ElementType)
-				{
-//				VerticalPanel PanelA = new VerticalPanel();
-//				PanelA.setSize("100%", "100%");
-				Pestanas.put((ElementType) MetaElem, PanelA);
-				MetaOrden.add((ElementType) MetaElem);
-				}
-			else 
-				{
-				 procesaEstrella((Iterator)MetaElem);
-				}
-			
-
 		}
 		
 	}
 
+
+
+
+//
 //	/**
-//	 * @return the basePath
+//	 * Funcion que procesa un recurso, creando el padre si este no existe.
+//	 * @param metaValueD
 //	 */
-//	public static String getBasePath() {
-//		return BasePath;
+//	private CompositeTabElement procesa(Element metaValueD) {
+//		
+//		if (Pestanas.containsKey(metaValueD.getHastype()))
+//		{
+//			VerticalPanel A = Pestanas.get(metaValueD.getHastype());
+//			CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
+//			A.add(nuevo);
+//			Procesados.put(metaValueD, nuevo);
+////			MetaPestanaATab.put(metaValueD.getHastype(), nuevo);
+//			return nuevo;
+//		}
+//		else
+//		{
+//			ElementType Padre;
+//			
+//			Padre=FindPadreSummaryAscendente(metaValueD, metaValueD.getHastype());
+//			
+//			if (Pestanas.containsKey(Padre))
+//				if (isSummaryMeta(Padre,metaValueD))
+//					{
+//					//Se retorno el padre por ser el padre
+//						return procesosobremipadre(metaValueD,Padre);
+//					}
+//				else
+//				{
+//					Pestanas.put((ElementType) metaValueD.getHastype(), PanelA);
+//					MetaOrden.add((ElementType) metaValueD.getHastype());
+//					CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
+//					PanelA.add(nuevo);
+//					Procesados.put(metaValueD, nuevo);
+//					return nuevo;
+//				}
+//			else
+//				{
+//				return procesosobremipadre(metaValueD,Padre);
+//				}
+//			
+//			
+//			
+//			}	
+//		
+//	}
+
+//
+//	/**
+//	 * procesa si el padre es visible
+//	 * @param padre
+//	 * @param metaValueD 
+//	 * @return
+//	 */
+//	private boolean isSummaryMeta(ElementType padre, Element metaValueD) {
+//		Element PadreMV=findMetaValue(padre, metaValueD.getAmbitos());
+//		if (PadreMV!=null)
+//			return Oda2013OperatinoalViewStaticFunctions.isSummary(PadreMV);
+//		else
+//			return Oda2013OperatinoalViewStaticFunctions.isSummary(padre);
+//				
+//	}
+//
+//
+
+
+//
+//
+//	/**
+//	 * Procesa un elemto sobre su padre
+//	 * @param metaValueD
+//	 * @param padre
+//	 * @return 
+//	 */
+//	private CompositeTabElement procesosobremipadre(Element metaValueD, ElementType padre) {
+//		Element PadreValue=findMetaValue(padre,metaValueD.getAmbitos());
+//		
+//		CompositeTabElement PadreTab;
+//		if (PadreValue==null)
+//		{
+//			
+//			Element MV=getMetavalueVacio(padre,metaValueD.getAmbitos());
+//			if (MV==null)
+//			{
+//				MV = new Element(padre);
+//				for (Integer iterable_element :metaValueD.getAmbitos()) {
+//					MV.getAmbitos().add(iterable_element.intValue());
+//				}
+//				vaciosCreados.add(MV);
+//				PadreTab=procesa(MV);
+//				
+//			}
+//			else
+//			{
+//				PadreTab=Procesados.get(MV);
+//			}
+//		
+//		}
+//		else{
+//		
+//		 PadreTab = Procesados.get(PadreValue);
+//
+//		 if (PadreTab==null)
+//				{
+//			 		PadreTab=procesa(PadreValue);
+//				}
+//		}
+//		
+//		VerticalPanel A = PadreTab.getPanelHijos();	
+//		CompositeTabElement nuevo= new CompositeTabElement(metaValueD);
+//		A.add(nuevo);
+//		Procesados.put(metaValueD, nuevo);
+//		return nuevo;
+//		
 //	}
 //
 //	/**
-//	 * @param basePath the basePath to set
+//	 * Busca al padre que ademas de se meta sea summary
+//	 * @param metaValueD
+//	 * @return
 //	 */
-//	public static void setBasePath(String basePath) {
-//		BasePath = basePath;
+//	private ElementType FindPadreSummaryAscendente(Element metaValueD,ElementType Metain) {
+//		
+//		ElementType Padre;
+//		
+//		if (Metain.getFather()==null)
+//			return Metain;
+//		
+//		if (Metain.getFather() instanceof ElementType)
+//			Padre=(ElementType) Metain.getFather();
+//		else 
+//			Padre=findMeta((Iterator) Metain.getFather());
+//		
+//		Element PadreMV=findMetaValue(Padre, metaValueD.getAmbitos());
+//		
+//		
+//		
+//		if (PadreMV!=null)
+//			if (Oda2013OperatinoalViewStaticFunctions.isSummary(PadreMV))
+//				return Padre;
+//			else 
+//				return FindPadreSummaryAscendente(PadreMV,PadreMV.getHastype());
+//		else
+//			if (Oda2013OperatinoalViewStaticFunctions.isSummary(Padre))
+//				return Padre;
+//			else 
+//				return FindPadreSummaryAscendente(metaValueD,Padre);
+//
+//	}
+//
+//
+//
+//
+//
+//	private Element getMetavalueVacio(ElementType padre, ArrayList<Integer> ambitos) {
+//		for (Element vaciosCandidato : vaciosCreados) {
+//			if ((vaciosCandidato.getHastype()==padre)&&(checkAmbitos(vaciosCandidato.getAmbitos(),ambitos)))
+//				return vaciosCandidato;
+//		}
+//		
+//		return null;
+//	}
+//
+//	/**
+//	 * Busca un MetaValue en la lista de recursos
+//	 * @param element
+//	 * @param AmbitosElementoBase
+//	 * @return
+//	 */
+//	private Element findMetaValue(ElementType element, ArrayList<Integer> AmbitosElementoBase) {
+//		for (Element metaValuePro : Recurso.getDescription()) {
+//			if ((metaValuePro.getHastype()==element)&&(checkAmbitos(metaValuePro.getAmbitos(),AmbitosElementoBase)))
+//				return metaValuePro;
+//		}
+//		return null;
+//	}
+//
+//	
+//	/**
+//	 * Chequea si las dos listas de ambitos coinciden, la segunda es la que ha de ser mayor ( mas profunda ) que la primera ( padre )
+//	 * @param ambitos ambitos del padre a chequear
+//	 * @param AmbitosElementoBase ambitos del hijo
+//	 * @return
+//	 */
+//	private boolean checkAmbitos(ArrayList<Integer> ambitos,
+//			ArrayList<Integer> AmbitosElementoBase) {
+//		
+//		if (ambitos.size()>AmbitosElementoBase.size())
+//			return false;
+//		
+//		for (int i = 0; i < AmbitosElementoBase.size(); i++) {
+//			
+//			if ((ambitos.size()>i)&&(AmbitosElementoBase.get(i)!=null)&&(ambitos.get(i)!=null)&&(AmbitosElementoBase.get(i).intValue()!=ambitos.get(i).intValue()))
+//				return false;
+//		}
+//		
+//		return true;
+//	}
+//
+//	/**
+//	 * Busca el padre del elemento en Meta mas cercano hacia arriba en el arbol
+//	 * @param IteradorProcesar
+//	 * @return Meta mas cercano hacia arrriba en el arbol.
+//	 */
+//	private ElementType findMeta(Iterator IteradorProcesar) {
+//		if (IteradorProcesar.getFather()==null)
+//			return null;
+//		if (IteradorProcesar.getFather() instanceof ElementType)
+//			return (ElementType) IteradorProcesar.getFather();
+//		else return findMeta((Iterator)IteradorProcesar.getFather());
+//	}
+//
+
+//	/**
+//	 * Mecanismo de proceso de un atributo estrella para la creacion de las pestañas
+//	 * @param metaElem
+//	 */
+//	private void procesaEstrella(Iterator metaElem) {
+//		for (Structure MetaElem : metaElem.getSons()) {
+//			
+//			if (MetaElem instanceof ElementType)
+//				{
+////				VerticalPanel PanelA = new VerticalPanel();
+////				PanelA.setSize("100%", "100%");
+//				Pestanas.put((ElementType) MetaElem, PanelA);
+//				MetaOrden.add((ElementType) MetaElem);
+//				}
+//			else 
+//				{
+//				 procesaEstrella((Iterator)MetaElem);
+//				}
+//			
+//
+//		}
+//		
 //	}
 
-
-	
 
 	@Override
 	protected void onLoad() {
---
+++ Oda2013/src/fdi/ucm/client/publiccolletions/CompositeDocumentDescriptionTabStructureSummary.java
@@ -0,0 +1,249 @@
+package fdi.ucm.client.publiccolletions;
+
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import fdi.ucm.client.controller.Oda2013OperatinoalViewStaticFunctions;
+import fdi.ucm.shared.model.collection.document.Documents;
+import fdi.ucm.shared.model.collection.document.Element;
+import fdi.ucm.shared.model.collection.document.LinkElement;
+import fdi.ucm.shared.model.collection.document.ResourceElementFile;
+import fdi.ucm.shared.model.collection.document.ResourceElementURL;
+import fdi.ucm.shared.model.collection.document.TextElement;
+import fdi.ucm.shared.model.collection.grammar.ElementType;
+import fdi.ucm.shared.model.collection.grammar.LinkElementType;
+import fdi.ucm.shared.model.collection.grammar.TextElementType;
+
+public class CompositeDocumentDescriptionTabStructureSummary extends Composite {
+
+	private static final String ICONOS_KEYICON_PNG = "Iconos/Keyicon.png";
+	private static final String TWO_POINTS = ":";
+	private VerticalPanel PanelHijos;
+//	private static List<Structure> ColeccionSons;
+	private static String ImagenAsociada;
+	private Image MRVI;
+	private Label LabelType;
+	
+	private ElementType Elemento;
+	private Documents Recurso;
+	private ArrayList<Integer> Ambitos;
+	private boolean Active;
+	
+	
+	public CompositeDocumentDescriptionTabStructureSummary(
+			ElementType metaValue, Documents documentoG,
+			ArrayList<Integer> ambitos) {
+		Elemento=metaValue;
+		
+		Recurso=documentoG;
+		
+		Ambitos	= ambitos;
+		
+		Active = false;
+		
+		VerticalPanel verticalPanel = new VerticalPanel();
+		initWidget(verticalPanel);
+
+			HorizontalPanel ElementPanel = new HorizontalPanel();
+			verticalPanel.add(ElementPanel);
+			ElementPanel.setSize("", "100%");
+			
+			
+			Image image = new Image(ICONOS_KEYICON_PNG);
+			ElementPanel.add(image);
+			image.setSize("25px", "22px");
+			
+			Element MV=getMetaValue((ElementType)Elemento);
+			
+			if (MV==null)
+				{
+				MV=generanuevo((ElementType)Elemento);
+				MV.setAmbitos(Ambitos);
+				ElementPanel.add(getElementbase((ElementType)Elemento));
+				}
+			else 
+				{
+				
+				ElementPanel.add(getElementbase(MV));
+				}
+
+			HorizontalPanel horizontalPanel = new HorizontalPanel();
+			verticalPanel.add(horizontalPanel);
+			
+			SimplePanel Glue = new SimplePanel();
+			horizontalPanel.add(Glue);
+			Glue.setWidth("35px");
+			
+			PanelHijos = new VerticalPanel();
+			horizontalPanel.add(PanelHijos);
+		
+		
+	}
+
+	
+	
+	
+
+
+
+	
+
+	/**
+	 * Obtiene el MetaValor Asociado y sino crea uno
+	 * @param elemento1
+	 * @return
+	 */
+	private Element getMetaValue(ElementType elemento1) {
+		for (Element element : Recurso.getDescription()) {
+			if (element.getHastype()==elemento1&&EqualsAmbitos(element.getAmbitos()))
+				return element;
+			
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Metodo que genera el metavbalueNuevo
+	 * @param elemento1
+	 * @return
+	 */
+	private Element generanuevo(ElementType elemento1) {
+		if (elemento1 instanceof TextElementType)
+			return new TextElement((TextElementType) elemento1);
+		if (elemento1 instanceof LinkElementType)
+			return new LinkElement((LinkElementType) elemento1);
+		return new Element(elemento1);
+	}
+	
+	
+	
+	
+	
+	/**
+	 * Test Si los ambitos son iguales
+	 * @param ambitos2
+	 * @return
+	 */
+	private boolean EqualsAmbitos(ArrayList<Integer> ambitos2) {
+		if (ambitos2.size()<Ambitos.size())
+			return false;
+		for (int i = 0; i < Ambitos.size(); i++) {
+			if (!Ambitos.get(i).equals(ambitos2.get(i)))
+				return false;
+		}
+		return true;
+	}
+	
+	
+	
+
+	/**
+	 * Funcion que genera el elemento asociado a el valor
+	 * @param elemento2 
+	 * @param metaValueD elemento de valor de entrada
+	 * @return widget asociado al valor
+	 */
+	private Widget getElementbase(fdi.ucm.shared.model.collection.grammar.ElementType elemento2) {
+		HorizontalPanel V=new HorizontalPanel();
+		LabelType = new Label(elemento2.getName()+TWO_POINTS);
+		V.add(LabelType);
+		return V;
+	}
+	
+	/**
+	 * Funcion que genera el elemento asociado a el valor
+	 * @param metaValueD elemento de valor de entrada
+	 * @return widget asociado al valor
+	 */
+	private Widget getElementbase(Element metaValueD) {
+		HorizontalPanel V=new HorizontalPanel();
+		LabelType = new Label(metaValueD.getHastype().getName()+TWO_POINTS);
+		V.add(LabelType);
+		
+		Widget Result=null;
+		
+		if (metaValueD instanceof TextElement)
+			{
+			Result=new Label(((TextElement) metaValueD).getValue());
+			
+			}
+		else if (metaValueD instanceof ResourceElementFile)
+			{
+				ImagenAsociada=Oda2013OperatinoalViewStaticFunctions.calculaImagenAsociada(((ResourceElementFile)metaValueD).getValue());
+			
+			MRVI=new ImageMetaRelationResourceValue(((ResourceElementFile)metaValueD).getValue(),ImagenAsociada);
+			
+			Result= MRVI;	
+			}
+		else if (metaValueD instanceof ResourceElementURL)
+		{
+			ImagenAsociada=Oda2013OperatinoalViewStaticFunctions.calculaImagenAsociada(((ResourceElementURL)metaValueD).getValue());
+		
+		MRVI=new ImageMetaRelationResourceValue(((ResourceElementURL)metaValueD).getValue(),ImagenAsociada);
+		
+		Result= MRVI;	
+		}
+			else 
+				if (metaValueD instanceof LinkElement)
+				{
+				ImagenAsociada=Oda2013OperatinoalViewStaticFunctions.calculaImagenAsociada(((LinkElement)metaValueD).getValue());
+				
+				MRVI=new ImageMetaRelationConstructValue(((LinkElement)metaValueD).getValue(),ImagenAsociada);
+				
+				Result= MRVI;	
+				} 
+		
+		
+		if (Result!=null)
+			V.add(Result);
+		
+		
+		return V;
+	}
+
+	
+
+	
+	
+	/**
+	 * @return the panelHijos
+	 */
+	public VerticalPanel getPanelHijos() {
+		return PanelHijos;
+	}
+
+
+
+
+
+
+
+
+
+	/**
+	 * @param panelHijos the panelHijos to set
+	 */
+	public void setPanelHijos(VerticalPanel panelHijos) {
+		PanelHijos = panelHijos;
+	}
+
+
+
+
+
+
+
+
+
+	public boolean isActive() {
+		return Active;
+	}
+}
